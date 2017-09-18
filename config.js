f765d9981d456a05e6d2a2c162c9ddd26998a8f2
Pyry Pajunpää <pymapa@utu.fi>
2017-09-18 22:00:53 +0300

    config stuff piiloon

--- config.js
+++ config.js
@@ -2,11 +2,7 @@ module.exports = {
     //Paytrail
     kauppiastunnus:process.env.KAUPPIASTUNNUS || '13466',
     kauppiasvarmenne: process.env.KAUPPIASVARMENNE || '6pKF4jkv97zmqBJ3ZL8gUw5DfT2NMQ',
-
-    //Mailgun
-    mailgun_api_key: 'key-c296edfe248aea2b7abb9f83acd5f74b',
-    domain: 'hybridispeksi.fi',
-
+    
     //Hinnasto
     sprice: 12,
     nprice: 14,
