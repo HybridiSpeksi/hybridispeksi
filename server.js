bd4a81e82b9473882d2b582f8d3bb02c22025e90
Pyry Pajunpää <pymapa@utu.fi>
2017-09-19 09:51:55 +0300

    morgan

--- server.js
+++ server.js
@@ -1,27 +1,38 @@
-// dev-server.js
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
 const config = require('./config');
 const mongoose = require('mongoose');
+const morgan = require("morgan");
+const path = require('path');
 
+// Basic conf
 dotenv.load();
+app.set('port', process.env.PORT || 3001);
+
+if (process.env.NODE_ENV !== "production") {
+    app.use(morgan('dev'));
+} else {
+    app.use(morgan('common'))
+}
 
 mongoose.connect(process.env.MONGOLAB_URI);
-mongoose.connection.on('connected', function() {
+mongoose.connection.on('connected', function () {
     console.log('Connected to database in ' + process.env.MONGOLAB_URI);
 })
 
-app.set('port', process.env.PORT || 3001);
+// Initiate API
+app.use('/api', require('./api/index'));
 
-const index = require('./api/index');
+// Static assets
+app.use('/', express.static(path.join(__dirname + '/dist')));
+app.use('/assets', express.static(path.join(__dirname + 'assets')));
 
-app.use('/api', index);
-
-app.get("*", function(req, res) {
-
+// Serve index.html for every other request. Client router handles the rest
+app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 })
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), function () {
     console.log('Node App Started on port ' + app.get('port'));
 });
