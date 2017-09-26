1a30294d2917ac5986f730672692529408a55349
Tatu Heinonen <tatahe@utu.fi>
2017-09-26 14:23:24 +0300

    asd

--- client/components/Home/Home.js
+++ client/components/Home/Home.js
@@ -48,11 +48,13 @@ class Home extends Component {
                         Eri speksiryhmien innokkuus toteuttaa omstarteja vaihtelee paljon.</p> */}
                     </div>
                 </div>
+                {/*
                 <div className={"row align-items-center justify-content-center " + styles.content_contact}>
                     <div className={"col-sm-8 " + styles.contact_box}>
                         <h1>Yhteystiedot</h1>
                     </div>
                 </div>
+                */}
             </div>
 
                     /*
