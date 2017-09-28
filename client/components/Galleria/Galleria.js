56dfb340e2181d2c8e002114595ed7657cd8aaba
Tatu Heinonen <tatahe@utu.fi>
2017-09-28 12:37:27 +0300

    Changes to Esitykset and Galleria

--- client/components/Galleria/Galleria.js
+++ client/components/Galleria/Galleria.js
@@ -11,14 +11,12 @@ class Galleria extends Component {
     return (
       <div className={"container-fluid " + styles.container}>
         <div className={"row align-items-center " + styles.content}>
-          <div className={"row justify-content-center " + styles.content_row}>
             <div className={"col-sm-6 col-11 " + styles.galleria_desc}>
               <h1>Galleria</h1>
               <p>Tänne tulee kuvei.</p>
               <p>Tästä lisää infoa myöhemmin.</p>
               <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus cursus ipsum nec imperdiet cursus. Nulla vel dui nulla. Etiam urna libero, rhoncus id finibus eget, pulvinar nec ante. In sollicitudin sem et velit posuere, et malesuada ligula venenatis. Mauris vel augue ac nisi pretium consectetur. Sed a massa ligula. Sed dapibus non ex at eleifend. Donec vel lacinia neque. Sed commodo varius massa vel lacinia.</p>
             </div>
-          </div>
         </div>
       </div>
     )
