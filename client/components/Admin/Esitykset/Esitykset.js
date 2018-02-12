55ae8f44d52cdb2a346da6cc154e226c6f7d29ff
Pyry Pajunpää <pymapa@utu.fi>
2018-02-12 16:57:39 +0200

    esitykset-component added

---
+++ client/components/Admin/Esitykset/Esitykset.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import PropTypes from ''
+
+class EsityksetContainer extends Component {
+    render () {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+EsityksetContainer.propTypes = {
+}
+
+export default EsityksetContainer;
