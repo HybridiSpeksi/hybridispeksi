ada70e4fdb11a75fd1f07b501772ad68b6f1a769
Pyry Pajunpää <pyry.pajunpaa@houston-inc.com>
2018-03-13 13:23:31 +0200

    jäsenrekisterin refaktorointia

--- client/components/Admin/Produktio/Jasentiedot.js
+++ client/components/Admin/Produktio/Jasentiedot.js
@@ -1,99 +1,137 @@
-import React, { Component } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 import { Text, Dropdown } from '../../Utils/Form';
 import styles from './Produktionjasenet.css';
 
-import auth from '../../Utils/Auth'
+import auth from '../../Utils/Auth';
 
-class Jasentiedot extends Component {
-
-    render() {
-        if (!this.props.jasen.tehtavat) {
-            this.props.jasen.tehtavat = []
-        }
-        let tehtavaValinnat = this.props.jasen.tehtavat.map((t, i) => {
-            if (t === "") {
-                t = "tyhja"
-            }
-            return (
-                <div className="col" key={i}>
-                    <Dropdown
-                        options={this.props.tehtavat}
-                        selected={t}
-                        id={i}
-                        label=""
-                        name={"tehtavat"}
-                        onChange={this.props.handleChange}
-                    />
-                    <button onClick={() => this.props.poistaTehtava(i)}><i className="fa fa-trash" aria-hidden="true"></i></button>
-                </div>
-            )
-        })
-        return (
-            <div className="row">
-                <div className={"col " + styles.jasentiedotlaatikko}>
-                    <div className="row">
-                        <div className="col">
-                            <h3>{this.props.jasen.fname} {this.props.jasen.sname}</h3>
-                        </div>
-                        <div className="col text-right">
-                            <button onClick={() => this.props.valitseJasen(false)} className="btn btn-default">
-                                <i className="fa fa-times" aria-hidden="true"></i>
-                            </button>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-sm-6">
-                            <Text
-                                value={this.props.jasen.email}
-                                name="email"
-                                type="email"
-                                onChange={this.props.handleChange}
-                                placeholder="Sähköposti"
-                                id="email-input"
-                                autofocus="false"
-                                label=""
-                            />
-                        </div>
-                        <div className="col-sm-6">
-                            <Text
-                                value={this.props.jasen.pnumber}
-                                name="pnumber"
-                                type="tel"
-                                onChange={this.props.handleChange}
-                                placeholder="Puh"
-                                id="pnumber-input"
-                                autofocus="false"
-                            />
-                        </div>
-                    </div>
-                    <div className="row">
-                        {tehtavaValinnat}
-
-                    </div>
-                    <div className="row">
-                        <div className="col"><button className="btn" onClick={this.props.lisaaTehtava}>Lisää tehtävä</button></div>
-                    </div>
-                    <div className="row">
-                        <div className="col">
-                            <p><i>"{this.props.jasen.lisatiedot}"</i></p>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col">
-                            <p>{this.props.jasen.jarjesto}</p>
-                        </div>
-                    </div>
-                    {this.props.henkilotiedotMuuttuneet ? <button className="btn btn-primary" onClick={() => this.props.tallennaMuutokset()}>Tallenna muutokset</button> : ""}
-                    {this.props.jasen.hakeeJaseneksi && !this.props.jasen.member ? <p>Hakee yhdistyksen jäseneksi</p> : ""}
-                    {this.props.jasen.hakeeJaseneksi && !this.props.jasen.member && auth.getUserRole() > 3 ? (
-                        <button className="btn btn-primary" data-toggle="modal" data-target="#uusijasen-modal">
-                            Hae {this.props.jasen.fname}lle yhdistyksen jäsenyyttä
-                        </button>
-                    ) : ""}
-                </div>
-            </div>
-        )
+const Jasentiedot = (props) => {
+  const {
+    jasen,
+    tehtavat,
+    handleChange,
+    henkilotiedotMuuttuneet,
+    tallennaMuutokset,
+    lisaaTehtava,
+    poistaTehtava,
+    valitseJasen,
+  } = props;
+  const getTehtavavalinnat = () => {
+    if (!jasen.tehtavat) {
+      jasen.tehtavat = [];
     }
-}
+    const tehtavaValinnat = jasen.tehtavat.map((t, i) => {
+      if (t === '') {
+        t = 'tyhja';
+      }
+      return (
+        <div className="col" key={i._id}>
+          <Dropdown
+            options={tehtavat}
+            selected={t}
+            id={i}
+            label=""
+            name="tehtavat"
+            onChange={handleChange}
+          />
+          <button onClick={() => poistaTehtava(i)}>
+            <i className="fa fa-trash" aria-hidden="true" />
+          </button>
+        </div>
+      );
+    });
+    return tehtavaValinnat;
+  };
 
-export default Jasentiedot
+  return (
+    <div className="row">
+      <div className={'col ' + styles.jasentiedotlaatikko}>
+        <div className="row">
+          <div className="col">
+            <h3>
+              {jasen.fname} {jasen.sname}
+            </h3>
+          </div>
+          <div className="col text-right">
+            <button onClick={() => valitseJasen(false)} className="btn btn-default">
+              <i className="fa fa-times" aria-hidden="true" />
+            </button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-sm-6">
+            <Text
+              value={jasen.email}
+              name="email"
+              type="email"
+              onChange={handleChange}
+              placeholder="Sähköposti"
+              id="email-input"
+              autofocus="false"
+              label=""
+            />
+          </div>
+          <div className="col-sm-6">
+            <Text
+              value={jasen.pnumber}
+              name="pnumber"
+              type="tel"
+              onChange={handleChange}
+              placeholder="Puh"
+              id="pnumber-input"
+              autofocus="false"
+            />
+          </div>
+        </div>
+        <div className="row">{getTehtavavalinnat}</div>
+        <div className="row">
+          <div className="col">
+            <button className="btn" onClick={lisaaTehtava}>
+              Lisää tehtävä
+            </button>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <p>
+              <i>"{jasen.lisatiedot}"</i>
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col">
+            <p>{jasen.jarjesto}</p>
+          </div>
+        </div>
+        {henkilotiedotMuuttuneet ? (
+          <button className="btn btn-primary" onClick={() => tallennaMuutokset()}>
+            Tallenna muutokset
+          </button>
+        ) : (
+          ''
+        )}
+        {jasen.hakeeJaseneksi && !jasen.member ? <p>Hakee yhdistyksen jäseneksi</p> : ''}
+        {jasen.hakeeJaseneksi && !jasen.member && auth.getUserRole() > 3 ? (
+          <button className="btn btn-primary" data-toggle="modal" data-target="#uusijasen-modal">
+            Hae {jasen.fname}lle yhdistyksen jäsenyyttä
+          </button>
+        ) : (
+          ''
+        )}
+      </div>
+    </div>
+  );
+};
+
+Jasentiedot.propTypes = {
+  jasen: PropTypes.object,
+  tehtavat: PropTypes.array,
+  handleChange: PropTypes.func,
+  tallennaMuutokset: PropTypes.func,
+  lisaaTehtava: PropTypes.func,
+  poistaTehtava: PropTypes.func,
+  henkilotiedotMuuttuneet: PropTypes.bool,
+  valitseJasen: PropTypes.func,
+};
+
+export default Jasentiedot;
