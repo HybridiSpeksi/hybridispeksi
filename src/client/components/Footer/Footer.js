744ca7f656472145919c92e18e47836e68065cff
Pyry <pyry.pajunpaa@gmail.com>
2018-11-29 21:52:39 +0200

    footer refactoring

--- src/client/components/Footer/Footer.js
+++ src/client/components/Footer/Footer.js
@@ -1,33 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 
 import styles from './Footer.css';
 import SomeIcons from './SomeIcons';
 
-class Footer extends Component {
-  addEmail() {
-    return (
-      <a href="mailto:hallitus@hybridispeksi.fi" className="navbar-brand">hallitus@hybridispeksi.fi</a>
-    );
-  }
-
-  render() {
-    return (
-      <div className={'container-fluid ' + styles.container}>
-        <div>
-          <nav className="navbar navbar-inverse">
-            <Link className="navbar-brand" to="/">HybridiSpeksi</Link>
-          </nav>
-        </div>
-        <SomeIcons />
-        <div>
-          <nav className="navbar navbar-inverse">
-            {this.addEmail()}
-          </nav>
-        </div>
-      </div>
-    );
-  }
-}
+const Footer = () => (
+  <div className={'container-fluid ' + styles.container}>
+    <div>
+      <nav className="navbar navbar-inverse">
+        <Link className="navbar-brand" to="/">HybridiSpeksi</Link>
+      </nav>
+    </div>
+    <SomeIcons />
+    <div>
+      <nav className="navbar navbar-inverse">
+        <a href="mailto:hallitus@hybridispeksi.fi" className="navbar-brand">hallitus@hybridispeksi.fi</a>
+      </nav>
+    </div>
+  </div>
+);
 
 export default Footer;
