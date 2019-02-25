14de49157e10aac47ab1fda95310323338be409d
Tatu Heinonen <tatahe@utu.fi>
2019-02-25 21:57:18 +0200

    Fixed layout to more coherent

--- src/client/components/Speksi2019/SpeksiHero.js
+++ src/client/components/Speksi2019/SpeksiHero.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import styles from './SpeksiHero.css';
-import HeroFullViewport from '../HeroFullViewport/HeroFullViewport';
 
 const Hero = () => (
-  <HeroFullViewport>
-    <div className={styles.headerContainer}>
+  <div className={styles.container}>
+    <div className={styles.headerTexts}>
       <h1 className={`${styles.bigsubHeading}`}>Hybridispeksi 2019</h1>
       <h1 className={`${styles.bigHeading}`}>Viimeinen Lohikäärmeisku</h1>
       <p className={`${styles.subHeading}`}>
-        Lipunmyynti aukeaa keskiviikkona 27.2.
+          Lipunmyynti aukeaa keskiviikkona 27.2.
       </p>
-      <div className={`${styles.instructions}`}>
+    </div>
+    <div className={`${styles.instructionsContainer}`}>
+      <div className={styles.instructions}>
         <h3>Ohjeet lipunmyyntiin</h3>
         <ol className={`${styles.instructions_list}`}>
           <li>Valitse haluamasi näytös</li>
@@ -20,7 +21,7 @@ const Hero = () => (
         </ol>
       </div>
     </div>
-  </HeroFullViewport>
+  </div>
 );
 
 export default Hero;
